2feec11eff9316ce980ed007470f5143f4ef0033
BintangQurne <bintangqurne7@gmail.com>
2024-12-24 13:36:17 +0700

    update install

--- TPM/src/main.tsx
+++ TPM/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider } from 'react-router-dom'
+import router from './router.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode >,
 )
